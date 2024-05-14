f993a3e0a9e5f9fbcc200b7ebd877a7130f422a2
michalsuwala <flks126@protonmail.com>
2024-05-14 12:48:12 +0200

    second

--- Observer.h
+++ Observer.h
@@ -1,19 +1,15 @@
-// Observer.h
-
 #ifndef _OBSERVER_H
 #define _OBSERVER_H
 
-#include <string> // for std::string
+#include <string>
 
 class Observer {
 public:
-    // Function declarations
     std::string getObserver(int id);
     void addObserver(std::string name, int id);
 private:
-    // Member variables
     std::string name;
     int id;
 };
 
-#endif  // _OBSERVER_H
+#endif  
--- Year.h
+++ Year.h
@@ -3,21 +3,19 @@
 #ifndef _YEAR_H
 #define _YEAR_H
 
-#include <string> // for std::string
-#include <vector> // for std::vector
+#include <string> 
+#include <vector>
 #include "Observation.h"
 #include "Species.h"
 
 class Year {
 public:
-    // Function declarations
     std::string getYear(int number);
     void addYear(int number, std::string speciesName);
 private:
-    // Member variables
     int number;
     std::vector<Observation> observations;
     Species species;
 };
 
-#endif  // _YEAR_H
+#endif 
