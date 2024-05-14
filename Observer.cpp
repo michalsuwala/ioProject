55f2bb870febc84b91cdb1ef6ec3da5a1fd5e525
BartekSliwinski <121253906+BartekSliwinski@users.noreply.github.com>
2024-05-14 12:59:08 +0200

    hej

--- Observer.cpp
+++ Observer.cpp
@@ -1,19 +1,17 @@
-// Observer.cpp
+
 
 #include "Observer.h"
-#include <iostream> // for cout
+#include <iostream> 
 
-// Implementation of getObserver function
+
 std::string Observer::getObserver(int id) {
-    // In a real application, you would fetch observer details from a database or some other data source.
-    // For now, let's just print a message.
+   
     std::cout << "Getting observer with ID: " << id << "\n";
-    return ""; // Placeholder return value
+    return ""; 
 }
 
-// Implementation of addObserver function
+
 void Observer::addObserver(std::string name, int id) {
-    // In a real application, you would add observer details to a database or some other data storage mechanism.
-    // For now, let's just print a message.
+
     std::cout << "Adding observer: " << name << " with ID: " << id << "\n";
 }
